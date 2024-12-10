dd5a4e71b6fa631f253e68113e5a0726a4bf97bd
Kunanon <kunanon@drivehub.co>
2024-12-10 17:09:08 +0700

    setup database and initial first api
    - create api get columns
    - create api get cards

--- server.js
+++ server.js
@@ -1,31 +1,14 @@
 const express = require("express");
-
-const app = express();
-const mysql = require("mysql");
 const bodyParser = require("body-parser");
-
-require("dotenv").config();
+const app = express();
 
 app.use(bodyParser.urlencoded());
+
 app.use("/api/board/", require("./api/api_board"));
 app.use("/api/auth/", require("./api/api_auth"));
 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-});
+const port = 8085;
 
-connection.connect((err) => {
-  if (err) {
-    console.log("Error connection: ", err);
-    return;
-  }
-  console.log("Connection success");
-});
-
-app.listen(8085, () => {
-  console.log("Server is running");
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
