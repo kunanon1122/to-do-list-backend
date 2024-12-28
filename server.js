d4a2d66e10a7e6a11947e06a628c1a8653dac6ec
Kunanon <kunanon@drivehub.co>
2024-12-28 16:25:44 +0700

    update port env

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use("/api/column/", require("./api/api_column"));
 app.use("/api/card/", require("./api/api_card"));
 app.use("/api/auth/", require("./api/api_auth"));
 
-const port = 8085;
+const port = process.env.PORT || 8085;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
