b21bb8fed81dc1b6f685b1d6a47458e318e6a497
Kunanon <kunanon@drivehub.co>
2024-12-30 23:08:19 +0700

    update api /health for health check

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ app.use(cors());
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 
+// Define a health check endpoint
+app.use("/health", (req, res) => {
+  res.status(200).send("OK");
+});
+
 app.use("/api/column/", require("./api/api_column"));
 app.use("/api/card/", require("./api/api_card"));
 app.use("/api/auth/", require("./api/api_auth"));
